909183ae7bb34510f3811d763685cf3305ad132b
Manuel Lera Ramirez <manulera14@gmail.com>
2022-11-02 17:53:21 +0000

    misc_feature excluded from only_modified_coordinates.tsv

--- get_modifications_on_main_features_only.py
+++ get_modifications_on_main_features_only.py
@@ -6,7 +6,7 @@ data = pandas.concat([
     pandas.read_csv('pre_svn_coordinate_changes_file.tsv', delimiter='\t', na_filter=False)
 ])
 # We only consider CDS features in genes that have alleles with sequence errors
-output_data = data[~data['feature_type'].isin(["5'UTR","3'UTR",'intron','promoter','LTR'])]
+output_data = data[~data['feature_type'].isin(["5'UTR","3'UTR",'intron','promoter','LTR', 'misc_feature'])]
 
 # See the columns that only differ in value and added_removed > coordinates were modified
 d = output_data.drop(columns=['value', 'added_or_removed'])
