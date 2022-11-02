1fff2ea7d6d07991aefde31918f54bb1b9e50bd2
Manuel Lera Ramirez <manulera14@gmail.com>
2022-11-02 15:52:13 +0000

    added functionality to show only the modification changes

---
+++ get_modifications_on_main_features_only.py
@@ -0,0 +1,15 @@
+import pandas
+
+# Load coordinate changes
+data = pandas.concat([
+    pandas.read_csv('all_coordinate_changes_file.tsv', delimiter='\t', na_filter=False),
+    pandas.read_csv('pre_svn_coordinate_changes_file.tsv', delimiter='\t', na_filter=False)
+])
+# We only consider CDS features in genes that have alleles with sequence errors
+output_data = data[~data['feature_type'].isin(["5'UTR","3'UTR",'intron','promoter','LTR'])]
+
+# See the columns that only differ in value and added_removed > coordinates were modified
+d = output_data.drop(columns=['value', 'added_or_removed'])
+logi = d.duplicated(keep=False)
+
+output_data[logi].to_csv('only_modified_coordinates.tsv', sep='\t')
