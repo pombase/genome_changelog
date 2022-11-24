e12cc2550b2afae3c2732cf701aec399e0d61de4
manulera <manulera14@gmail.com>
2022-11-24 17:17:46 +0000

    getting started

---
+++ get_data_gene_changes_comments_and_pmids.sh
@@ -0,0 +1,12 @@
+"""
+Downloads the record of genome changes that was manually kept.
+"""
+
+mkdir -p gene_changes_comments_and_pmids/
+
+# rename the columns
+echo 'systematic_id	primary_name	old_coordinates	new_coordinates	comments	reference	date' > gene_changes_comments_and_pmids/gene-coordinate-change-data.tsv
+curl -k https://raw.githubusercontent.com/pombase/curation/master/data_files/gene-coordinate-change-data.tsv|tail -n+2 >> gene_changes_comments_and_pmids/gene-coordinate-change-data.tsv
+
+curl -L https://github.com/pombase/genome_changelog/releases/download/v0.1/all_qualifier_changes_file.tsv|grep '	db_xref	' > gene_changes_comments_and_pmids/all_qualifier_changes_file.tsv
+curl -L https://github.com/pombase/genome_changelog/releases/download/v0.1/pre_svn_qualifier_changes_file.tsv|grep '	db_xref	' > gene_changes_comments_and_pmids/pre_svn_qualifier_changes_file.tsv
