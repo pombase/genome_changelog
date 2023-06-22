3bed3ab165cb72b58972d4bb601738fe123e81c6
Manuel Lera Ramirez <manulera14@gmail.com>
2023-06-22 14:57:39 +0100

    fix action

--- gene_characterisation_status/update_characterisation.sh
+++ gene_characterisation_status/update_characterisation.sh
@@ -1,9 +1,9 @@
 set -e
 
 # This file is needed
-mkdir -p valid_ids_data
-echo 'systematic_id	primary_name	synonyms' > valid_ids_data/gene_IDs_names.tsv
-curl -k https://www.pombase.org/data/names_and_identifiers/gene_IDs_names.tsv | tail -n+2 >> valid_ids_data/gene_IDs_names.tsv
+mkdir -p ../valid_ids_data
+echo 'systematic_id	primary_name	synonyms' > ../valid_ids_data/gene_IDs_names.tsv
+curl -k https://www.pombase.org/data/names_and_identifiers/gene_IDs_names.tsv | tail -n+2 >> ../valid_ids_data/gene_IDs_names.tsv
 
 # Delete old data
 rm -rf data
