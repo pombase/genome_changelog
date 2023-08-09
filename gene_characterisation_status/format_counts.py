b9588bc12c85c8aa0ce820409eece47237a55b1a
Manuel Lera Ramirez <manulera14@gmail.com>
2023-08-09 09:51:36 +0200

    move colour_settings out of script

--- gene_characterisation_status/format_counts.py
+++ gene_characterisation_status/format_counts.py
@@ -1,6 +1,10 @@
 import pandas
 import matplotlib.pyplot as plt
 import datetime
+import json
+
+with open('color_settings.json') as f:
+    color_settings = json.load(f)
 
 all_data = pandas.read_csv('results/merged_counts.tsv', delimiter='\t')
 all_data['date'] = pandas.to_datetime(all_data['date'], utc=True).dt.date
@@ -15,10 +19,9 @@ all_data['fission_yeast_unknown'] = all_data['counts'].apply(lambda x: x[8] + x[
 all_data.drop(columns=['counts'], inplace=True)
 all_data.to_csv('results/formatted_counts.tsv', sep='\t', index=False)
 
-colors = ['#FB363B', '#FD852C', '#F0EDBF', '#88bb92' ]
-# colors = ['#f6565a', '#fdb42c', '#a4e4b0']
+labels = [ 'Published', 'Inferred role', 'Conserved unknown', 'Fission yeast unknown']
+colors = [color_settings[x] for x in labels]
 
-labels = ['Published', 'Inferred role', 'Conserved unknown', 'Fission yeast unknown']
 # Plot a stackplot of the counts
 plt.figure()
 plt.stackplot(all_data['date'], all_data['published'], all_data['inferred_role'], all_data['conserved_unknown'], all_data['fission_yeast_unknown'], labels=labels, colors=colors)
