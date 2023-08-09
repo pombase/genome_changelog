05f7f05cd40c1bc3e5794c56cbbae1e6c28d67d7
Manuel Lera Ramirez <manulera14@gmail.com>
2023-08-09 09:46:29 +0200

    changed colours

--- gene_characterisation_status/format_counts.py
+++ gene_characterisation_status/format_counts.py
@@ -15,7 +15,7 @@ all_data['fission_yeast_unknown'] = all_data['counts'].apply(lambda x: x[8] + x[
 all_data.drop(columns=['counts'], inplace=True)
 all_data.to_csv('results/formatted_counts.tsv', sep='\t', index=False)
 
-colors = ['#FB363B', '#FD852C', '#FDED2C', '#20C040' ]
+colors = ['#FB363B', '#FD852C', '#F0EDBF', '#88bb92' ]
 # colors = ['#f6565a', '#fdb42c', '#a4e4b0']
 
 labels = ['Published', 'Inferred role', 'Conserved unknown', 'Fission yeast unknown']
