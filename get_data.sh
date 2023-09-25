010ae0b7235c90c65934060cb16ddf82220c07a1
Manuel Lera Ramirez <manulera14@gmail.com>
2023-09-25 12:08:42 +0100

    minor fix to get_get_data

--- get_data.sh
+++ get_data.sh
@@ -1,6 +1,6 @@
 mkdir -p valid_ids_data
 echo 'systematic_id	primary_name	synonyms' > valid_ids_data/gene_IDs_names.tsv
-curl -k https://www.pombase.org/data/names_and_identifiers/gene_IDs_names.tsv | tail -n+2 >> valid_ids_data/gene_IDs_names.tsv
+curl -k https://www.pombase.org/data/names_and_identifiers/gene_IDs_names.tsv | tail -n+3 >> valid_ids_data/gene_IDs_names.tsv
 
 # qualifier_changes from pre-svn downloaded from release
 curl -L https://github.com/pombase/genome_changelog/releases/latest/download/pre_svn_qualifier_changes_file.tsv > gene_changes_comments_and_pmids/pre_svn_qualifier_changes_file.tsv
